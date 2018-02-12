e43bbfef2c956e96fefdd47f6079e070283195ee
xfuaf <xfuaf@CSL4WK40>
2018-02-12 21:15:50 +0800

    CircleBrush Completed.

---
+++ CircleBrush.cpp
@@ -0,0 +1,65 @@
+//
+// CircleBrush.cpp
+//
+// The implementation of Circle Brush.
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "circlebrush.h"
+
+extern float frand();
+
+CircleBrush::CircleBrush(ImpressionistDoc* pDoc, char* name) :
+	ImpBrush(pDoc, name)
+{
+}
+
+void CircleBrush::BrushBegin(const Point source, const Point target)
+{
+	BrushMove(source, target);
+}
+
+void CircleBrush::BrushMove(const Point source, const Point target)
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg = pDoc->m_pUI;
+
+	if (pDoc == NULL) {
+		printf("CircleBrush::BrushMove  document is NULL\n");
+		return;
+	}
+
+	int size = pDoc->getSize() > 1 ? pDoc->getSize() : 1;
+	float r = float(size / 2.0);
+	float theta = 2 * float(3.1415) / float(50);
+	float tan_component = tanf(theta);
+	float radial_component = cosf(theta);
+	
+	float x = r;
+	float y = 0;
+
+	glBegin(GL_POLYGON);
+	SetColor(source);
+	
+	for (int i = 0; i < 50; i++) {
+		glVertex2d(target.x + x, target.y + y);
+		
+		float tx = -y;
+		float ty = x;
+		
+		x += tx * tan_component;
+		y += ty * tan_component;
+
+		x *= radial_component;
+		y *= radial_component;
+	}
+
+	glEnd();
+}
+
+void CircleBrush::BrushEnd(const Point source, const Point target)
+{
+	// do nothing so far
+}
+
